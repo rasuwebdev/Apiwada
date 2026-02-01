991f35f530975792bacfe3bc7d2379c72d42a5c3
rasuwebdev <apiwadaphysics.lk@gmail.com>
2026-02-01 22:36:53 +0530

    Create firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_hDpsNq60hwsEYGIAWFkn7LgLYPoD2D8",
+  authDomain: "apiwada.firebaseapp.com",
+  projectId: "apiwada",
+  storageBucket: "apiwada.firebasestorage.app",
+  messagingSenderId: "1077007378629",
+  appId: "1:1077007378629:web:c1f9be4b951770b29ee8ac",
+  measurementId: "G-71ZETYBFM0"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app); // This is your database connection
+export const analytics = getAnalytics(app);
